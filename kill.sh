3041660abc9a03efe78a20cad272c21236f34e73
Julian Baumgartner <jbaumgartner93@gmail.com>
2024-10-07 19:10:16 -0400

    Update

--- kill.sh
+++ kill.sh
@@ -1,4 +1,5 @@
-helm uninstall traefik traefik/traefik &&
+kubectl delete namespaces traefik
 kubectl delete -f deployment/deployment_go_app.yaml
+kubectl delete -f deployment/deployment_ringo.yaml
 kubectl delete -f deployment/deployment_python_app.yaml
 kubectl delete -f deployment/deployment_postgres.yaml
--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.1/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.1/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+
+kubectl create namespace traefik
 helm repo add traefik https://traefik.github.io/charts &&
-helm repo update &&
-helm install traefik traefik/traefik &&
-kubectl apply -f deployment/deployment_go_app.yaml
+	helm repo update &&
+	helm install traefik traefik/traefik --namespace traefik --set dashboard.enabled=true &&
+	kubectl apply -f deployment/deployment_go_app.yaml
+kubectl apply -f deployment/deployment_ringo.yaml
 kubectl apply -f deployment/deployment_python_app.yaml
 kubectl apply -f deployment/deployment_postgres.yaml
